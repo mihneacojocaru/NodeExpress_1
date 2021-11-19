be6e12c0c580df49b34753f53e9dbf5acd4703d5
mihneacojocaru <mihnea.co@gmail.com>
2021-11-19 15:14:27 +0100

    get post delete

--- Api/index.js
+++ Api/index.js
@@ -15,18 +15,55 @@ app.use(express.urlencoded({extended:false}));
 
 
 
+let d=data();
+
 app.get('/',(req,res)=>{
 
-     res.json(data());
+     res.json(d);
 
 });
 
+
+app.post('/',(req,res)=>{
+    let user=req.body;
+    d.push(user);
+
+    res.json("Am reusit");
+});
+
+app.delete('/:id',(req,res)=>{
+
+
+     let {id}=req.params;
+
+     d=d.filter(e=>e.id!=id);
+
+     res.json("ok");
+});
+
+app.put('/',(req,res)=>{
+
+    let user=req.body;
+
+    
+    for(let i=0;i<d.length;i++){
+
+        if(d[i].id==user.id){
+
+            d[i]=user;
+          
+        }
+    }
+   
+    res.json("from put")
+});
 
 app.listen(3000,()=>{
 
     console.log('App listenining on port 3000');
 
-})
+
+});
 
 
 
--- Client/viewHome.js
+++ Client/viewHome.js
@@ -1,78 +1,217 @@
-export default class ViewHome{
+export default class ViewHome {
+  constructor() {
+    this.header = document.getElementById("header");
+    this.header.innerHTML = this.setHeader();
 
-    constructor(){
-        this.header = document.getElementById("header");
-        this.header.innerHTML = this.setHeader();
+    this.root = document.getElementById("root");
+    this.root.innerHTML = this.createTable();
+    this.load();
 
-        this.root = document.getElementById("root");
-        this.root.innerHTML = this.createTable();
-    
-        this.load();
-    }
+    this.form = document.getElementById("personForm");
 
+    this.form.addEventListener("change", this.handleChange);
+    this.form.addEventListener("submit", this.handleSubmit);
 
-    load=async()=>{
+    this.person = {};
 
-        let data = await fetch("http://localhost:3000/");
+    this.table = document.getElementById("dataTable");
+    this.table.addEventListener("click", this.handleUpdateDelete);
+  }
 
-        let d=await data.json();
+  load = async () => {
+    let data = await fetch("http://localhost:3000/");
 
-        d.forEach(element => {
-            this.createTableEntry(element);   
-        });
+    let d = await data.json();
 
-    }
+    d.forEach((element) => {
+      this.createTableEntry(element);
+    });
+  };
 
-    createTableEntry = (obj) => {
-        let table = document.getElementById("table");
-        table.innerHTML += `<tr>
+  createTableEntry = (obj) => {
+    let table = document.getElementById("table");
+    table.innerHTML += `<tr>
                                 <th scope="row">${obj.id}</th>
                                 <td>${obj.first_name}</td>
                                 <td>${obj.last_name}</td>
                                 <td>${obj.email}</td>
+                                <td>
+                                <button id="btnUpdate" class="btn btn-success">Update</button>
+                                <button id="btnDelete" class="btn btn-danger">Delete</button>
+                                </td>
                             </tr>`;
-    }
+  };
 
-    createTable = () => {
-        return `<table class="table table-striped table-hover">
+  createTable = () => {
+    return `<table id="dataTable" class="table table-striped table-hover">
         <thead>
           <tr>
             <th scope="col">#</th>
             <th scope="col">First</th>
             <th scope="col">Last</th>
             <th scope="col">Email</th>
+            <th scope="col">Options</th>
           </tr>
         </thead>
         <tbody id="table"></tbody>
       </table>`;
-    }
+  };
 
-    setHeader = () => {
-        return `
+  setHeader = () => {
+    return `
         <div class="bg-dark text-light text-center py-1">
             <h1>Rest API</h1>
             <h2 class="text-success">Node Express</h2>
         </div>
         <div class="bg-light">
-            <form class="p-3">
+            <form id="personForm" class="p-3">
             <div class="form-group">
                 <label for="fNameInput">First Name</label>
-                <input type="email" class="form-control" id="fNameInput" aria-describedby="emailHelp" placeholder="Enter Frist Name">
+                <input type="text" class="form-control firstName" id="fNameInput" aria-describedby="emailHelp" placeholder="Enter Frist Name">
             </div>
             <div class="form-group">
                 <label for="lNameInput">Last Name</label>
-                <input type="email" class="form-control" id="lNameInput" aria-describedby="emailHelp" placeholder="Enter Last Name">
+                <input type="text" class="form-control lastName" id="lNameInput" aria-describedby="emailHelp" placeholder="Enter Last Name">
                 </div>
                 <div class="form-group">
                 <label for="emailInput">Email</label>
-                <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter Email">
+                <input type="email" class="form-control email" id="emailInput" aria-describedby="emailHelp" placeholder="Enter Email">
                 </div>
-            <button id="btnAdd" class="btn btn-primary my-1">Add</button>
-            <button id="btnUpdate" class="btn btn-success">Update</button>
-            <button id="btnDelete" class="btn btn-danger">Delete</button>
+            <button type="submit" id="btnAdd" class="btn btn-primary my-1">Add</button>
             </form>
         </div>
         `;
+  };
+
+  handleChange = (e) => {
+    let obj = e.target;
+
+    if (obj.classList.contains("firstName")) {
+      this.person.first_name = obj.value;
+    } else if (obj.classList.contains("lastName")) {
+      this.person.last_name = obj.value;
+    } else if (obj.classList.contains("email")) {
+      this.person.email = obj.value;
     }
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let options = {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify(this.person),
+      };
+
+      let data = await fetch("http://localhost:3000", options);
+
+      let d = await data.json();
+
+      if (d !== "Am reusit") {
+      } else {
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  handleUpdateDelete = async (e) => {
+    e.preventDefault();
+
+    let obj = e.target;
+    if (obj.id == "btnDelete") {
+      let objId = obj.parentElement.parentElement.children[0].textContent;
+
+      try {
+        let options = {
+          method: "DELETE",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+
+        let response = await fetch(`http://localhost:3000/${objId}`, options);
+
+        let r = await response.json();
+
+        console.log(r);
+
+        this.load();
+      } catch (error) {
+          console.warn(error);
+      }
+    }else if(obj.id == "btnUpdate"){
+        let fName = obj.parentElement.parentElement.children[1].textContent;
+        let lName = obj.parentElement.parentElement.children[2].textContent;
+        let email = obj.parentElement.parentElement.children[3].textContent;
+
+        let fNameBox = document.createElement("input");
+        fNameBox.classList.add("firstNameInput");
+        fNameBox.value = fName;
+        
+        let firstName = obj.parentElement.parentElement.children[1];
+        firstName.innerHTML = "";
+        firstName.appendChild(fNameBox);
+
+        let lNameBox = document.createElement("input");
+        lNameBox.classList.add("lastNameInput");
+        lNameBox.value = lName;
+        
+        let lastName = obj.parentElement.parentElement.children[2];
+        lastName.innerHTML = "";
+        lastName.appendChild(lNameBox);
+
+        let emailInput = document.createElement("input");
+        emailInput.classList.add("emailInput");
+        emailInput.value = email;
+        
+        let emailBox = obj.parentElement.parentElement.children[3];
+        emailBox.innerHTML = "";
+        emailBox.appendChild(emailInput);
+
+        obj.textContent = "Save";
+        obj.id = "btnSave";
+    } else if (obj.id == "btnSave"){
+        let fNameInput = document.querySelector(".firstNameInput");
+        let lNameInput = document.querySelector(".lastNameInput");
+        let emailInput = document.querySelector(".emailInput");
+
+        this.person.id = obj.parentElement.parentElement.children[0].textContent;
+        this.person.first_name = fNameInput.value;
+        this.person.last_name = lNameInput.value;
+        this.person.email = emailInput.value;
+
+    }
+  };
+
+  handleUpdate = async(e)=>{
+
+    e.preventDefault();
+    try {
+        let options = {
+            method:"PUT",
+            mode: "cors",
+            headers:{
+                "Content-Type": "application/json",
+            },
+
+            body: JSON.stringify(this.person)
+        };
+
+        let data = await fetch("http://localhost:3000",options);
+
+
+    } catch (error) {
+        console.warn(error);
+    }
+
+  }
+
 
 }
